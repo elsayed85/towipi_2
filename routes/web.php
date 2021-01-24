91226c80bf71bab62cf13776b322aeb09dfc6b40
unknown <elsayedkamal581999@gmail.com>
2021-01-24 19:38:38 +0200

    “wip”

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,8 @@
 <?php
 
-use App\Http\Settings\GeneralSettings;
-use App\Models\Product\Product;
-use App\Models\Site\Faq;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', "HomeController@index")->name('home');
-
 Auth::routes(['verify' => true]);
 Route::get('/p/{page:slug}', "PageController")->name('page');
 Route::get('faq', 'FaqController')->name('faq');
